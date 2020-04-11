47747bbed4e15a39c5420642fd6970d653c9b2de
Хомич Александр Ростислвович <khomych@dlit.dp.ua>
2020-04-11 13:36:54 +0300

    Add CompareVersions() method

---
+++ Lab_1/Version_Comparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Version_Comparer
+    {
+        public int CompareVersions(string str1, string str2)
+        {
+            int num1 = int.Parse(string.Concat(str1.Trim().Split('.')));
+            int num2 = int.Parse(string.Concat(str2.Trim().Split('.')));
+            if (num1 == num2 || Math.Max(num1, num2) / Math.Min(num1, num2) == 10)
+                return 0;
+            else if (num1 > num2)
+                return 1;
+            else
+                return -1;
+        }
+    }
+}
