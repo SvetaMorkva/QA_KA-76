8fb209ef766b472017b9dfc1be9976450b44b1a7
Хомич Александр Ростислвович <khomych@dlit.dp.ua>
2020-04-11 11:08:36 +0300

    Add Queue Implementation and mock class for tests

---
+++ Lab_1/CustomQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    public class CustomQueue<T>: List<T>
+    {
+        private object SyncRoot = new object();
+
+        public void Enqueue(T item)
+        {
+            lock (SyncRoot)
+                this.Add(item);
+        }
+
+        public T Dequeue()
+        {
+            T item = default(T);
+
+            lock (SyncRoot)
+                if (this.Count > 0)
+                {
+                    item = this[0];
+                    this.RemoveAt(0);
+                }
+            return (item);
+        }
+
+        public T Peek()
+        {
+            T item = default(T);
+
+            lock (this.SyncRoot)
+                if (this.Count > 0)
+                    item = this[0];
+            return (item);
+        }
+    }
+}
---
+++ Lab_1/Employees.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    public class Employee
+    {
+        public string Company { get; set; }
+        public string Contact { get; set; }
+        public string Country { get; set; }
+
+        public string String()
+        {
+            return string.Format("Company:{0} Contact:{1} Country:{2}", Company, Contact, Country);
+        }
+    }
+
+    public class Main
+    {
+        public static List<Employee> employees = new List<Employee>();
+        public static void AddEmployees()
+        {
+            employees.Add(new Employee() { Company = "Alfreds Futterkiste", Contact = "Maria Anders", Country = "Germany" });
+            employees.Add(new Employee() { Company = "Centro commercial Moctezuma", Contact = "Francisco Chang", Country = "Mexico" });
+            employees.Add(new Employee() { Company = "Ernst Handel", Contact = "Roland Mendel", Country = "Austria" });
+            employees.Add(new Employee() { Company = "Island Trading", Contact = "Helen Bennett", Country = "UK" });
+            employees.Add(new Employee() { Company = "Laughing Bacchus Winecellars", Contact = "Yoshi Tannamuri", Country = "Canada" });
+            employees.Add(new Employee() { Company = "Magazine Alimentari Riuniti", Contact = "Giovanni Rovelli", Country = "Italy" });
+            employees.Add(new Employee() { Company = "Island Trading", Contact = "Maria Mendel", Country = "USA" });
+        }
+    }
+}
