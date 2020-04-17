0c83cdce028a03f6652def83a4d38e2a5c2333ed
Petro Chynnyk <chynnyk@vivaldi.net>
2020-04-17 08:58:14 +0300

    Add task 2 in laboratory 1

---
+++ task_2/task_2/NUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Task2;
+
+namespace Task_2
+{
+    [TestFixture]
+    class NUnitTest
+    {
+        [TestCase ("1.2.3", "1.2.3", 0)]
+        [TestCase ("1.2.3", "4.5.6", -1)]
+        [TestCase ("1", "1.0", 0)]
+        [TestCase ("1.1.0", "1.0.1", 1)]
+
+
+        public void CompareVersion_InputString_ReturnInt(string version1, string version2, int expected)
+        {
+            Program program = new Program();
+            int result = program.CompareVersions(version1, version2);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ task_2/task_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        public int CompareVersions (string version1, string version2)
+        {
+            int[] v1 = new int[3] { 0, 0, 0 };
+            int[] v2 = new int[3] { 0, 0, 0 };
+            char spearator = '.';
+            string[] versionTmp1 = version1.Split(spearator);
+            string[] versionTmp2 = version2.Split(spearator);
+            int i = 0;
+            foreach (string s in versionTmp1)
+            {
+                v1[i] = int.Parse(s);
+                i++;
+            }
+            i = 0;
+            foreach (string s in versionTmp2)
+            {
+                v2[i] = int.Parse(s);
+                i++;
+            }
+            for(int j = 0; j < 3; j++)
+            {
+                if (v1[j] < v2[j])
+                    return -1;
+                else if (v1[j] > v2[j])
+                    return 1;
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
