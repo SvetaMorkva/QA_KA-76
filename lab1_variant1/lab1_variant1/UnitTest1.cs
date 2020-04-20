1660783ab5e0e7a7a21e0178723a5e802c7e8adf
Olha Pashnieva <olha.pashnieva@gmail.com>
2020-04-20 16:37:07 +0300

    initial commit Pashnieva

---
+++ lab1_variant1/lab1_variant1/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace lab1_variant1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
