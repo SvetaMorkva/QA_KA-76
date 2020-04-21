2f84b5c8555e11ff601cafa5a6b672e218f0e1ed
Pavel Pyvovar <jalovanfres@gmail.com>
2020-04-21 11:07:14 +0300

    VersionComparator completed and tested

--- lab1/test/DoublyLInkedListTest.cs
+++ lab1/test/DoublyLInkedListTest.cs
@@ -2,40 +2,42 @@ using System;
 using Xunit;
 using lab1;
 
-// namespace lab1
-// {
-    public class DoublyLinkedListTest
+public class DoublyLinkedListTest
+{
+    /*
+        Every method annotated with [Fact] will be marked as a test and run by xUnit.net
+        Fact methods cannot have parameters
+        The [Theory] attribute denotes a parameterised test
+    */
+    [Theory]
+    [InlineData(1)]
+
+    public void AddTest(int value)
     {
-        [Theory]
-        [InlineData(1)]
-        public void AddTest(int value)
-        {
-            Console.WriteLine("Testing DoublyLinkedList.Add");
-            var linked_list = new DoublyLinkedList(0);
-            linked_list.Add(value);
-            int new_head_key = linked_list.getHead().getKey();
-            Assert.Equal(new_head_key, value);
+        Console.WriteLine("Testing DoublyLinkedList.Add");
+        var linked_list = new DoublyLinkedList(0);
+        linked_list.Add(value);
+        int new_head_key = linked_list.getHead().getKey();
+        Assert.Equal(value, new_head_key);
 
-            linked_list.Add(2);
-            new_head_key = linked_list.getHead().getKey();
-            Assert.Equal(new_head_key, 2);
-        }
-
-        [Fact]
-
-        public void GetNextPrevTest()
-        {
-            Console.WriteLine("Testing Node.GetNext");
-            DoublyLinkedList linked_list = new DoublyLinkedList(0);
-            linked_list.Add(1);
-            linked_list.Add(2);
-            linked_list.Add(3);
-            Node curr = linked_list.GetCurrent(2);
-            Assert.Equal(curr.getKey(), 2);
-            int next = curr.getNext().getKey();
-            int prev = curr.getPrev().getKey();
-            Assert.Equal(next, 3);
-            Assert.Equal(prev, 1);
-        }
+        linked_list.Add(2);
+        new_head_key = linked_list.getHead().getKey();
+        Assert.Equal(2, new_head_key);
     }
-// }
+
+    [Fact]
+    public void GetNextPrevTest()
+    {
+        Console.WriteLine("Testing Node.GetNext");
+        DoublyLinkedList linked_list = new DoublyLinkedList(0);
+        linked_list.Add(1);
+        linked_list.Add(2);
+        linked_list.Add(3);
+        Node curr = linked_list.GetCurrent(2);
+        Assert.Equal(2, curr.getKey());
+        int next = curr.getNext().getKey();
+        int prev = curr.getPrev().getKey();
+        Assert.Equal(3, next);
+        Assert.Equal(1, prev);
+    }
+}
--- lab1/test/VersionComparatorTest.cs
+++ lab1/test/VersionComparatorTest.cs
@@ -2,25 +2,36 @@ using System;
 using Xunit;
 using lab1;
 
-// namespace lab1
-// {
-    public class VersionComparatorTest
+public class VersionComparatorTest
+{
+    /*
+        Every method annotated with [Fact] will be marked as a test and run by xUnit.net
+        Fact methods cannot have parameters
+        The [Theory] attribute denotes a parameterised test
+    */
+    
+    [Theory]
+    [InlineData("1.2.3", "4.5.6", -1)]
+    [InlineData("1", "1.0", 0)]
+    [InlineData("1.1.0", "1.0.1.", 1)]
+
+    public void compareVersionsTest(string version1, string version2, int expected)
     {
-        // Every method annotated with Fact will be marked as a test and run by xUnit.net
-        // [Fact]
-        // Fact methods cannot have parameters
-        // The [Theory] attribute denotes a parameterised test
-        
-        [Theory]
-        [InlineData("Pavlo", "Pyvovar", 1)]
-        public void CompareVersionsTest(string s1, string s2, int expected)
-        {
-            Console.WriteLine("I am in the CompareVersionsTest");
-            int actual = new VersionComparator().CompareVersions(s1, s2);
-
-            // Assert.AreEqual(expected, actual);
-            Assert.Equal(expected, actual);
-
-        }
+        Console.WriteLine("Testing VersionComparator.compareVersions");
+        int actual = new VersionComparator().compareVersions(version1, version2);
+        Assert.Equal(expected, actual);
     }
-// }
+
+    [Theory]
+    [InlineData("1.2.3", "1.2.3")]
+    [InlineData("1", "1.0.0")]
+    [InlineData("1.1.0", "1.1.0")]
+    [InlineData("1.", "1..0.0")]
+    public void completeVersionTest(string version, string expected)
+    {
+        Console.WriteLine("Testing VersionComparator.compareVersions");
+        VersionComparator vc = new VersionComparator();
+        string actual = vc.completeVersion(version);
+        Assert.Equal(expected, actual);
+    }
+}
