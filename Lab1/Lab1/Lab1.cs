69eff87312cf9f406a109eac91b78bee3153cbc7
Svitlana Morkva <sveta.morkva28@gmail.com>
2020-04-21 10:52:13 +0300

    [Lab1] added realization of custom queue and version compare task with unit test support
    Signed-off-by: Svitlana Morkva <sveta.morkva28@gmail.com>

---
+++ Lab1/Lab1/Lab1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QA_Lab1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+    public class Queue<T>
+    {
+        public void IsNullCheck(T item, string str = "queue is empty!")
+        {
+            if (item == null)
+            {
+                throw new NullReferenceException("Invalid action: " + str);
+            }
+        }
+        public void Enqueue(T item)
+        {
+            IsNullCheck(item, "trying to add unacceptable item!");
+            queue.Add(item);
+        }
+
+        public T Dequeue()
+        {
+            var item = queue.FirstOrDefault();
+
+            IsNullCheck(item);
+            queue.Remove(item);
+
+            return item;
+        }
+
+        public T Peek()
+        {
+            var item = queue.FirstOrDefault();
+            IsNullCheck(item);
+
+            return item;
+        }
+
+        public void Clear()
+        {
+            queue.Clear();
+        }
+        public int Count => queue.Count;
+        private List<T> queue = new List<T>();
+    }
+
+    public class VersionCompare
+    {
+        public static int Compare(string vers1, string vers2)
+        {
+            int[] arrVers1 = Array.ConvertAll(vers1.Split('.'), int.Parse);
+            int[] arrVers2 = Array.ConvertAll(vers2.Split('.'), int.Parse);
+            if (arrVers1.Length < arrVers2.Length)
+                AlignLength(arrVers1, arrVers2.Length);
+            else
+                AlignLength(arrVers2, arrVers1.Length);
+            for (int i = 0; i < arrVers1.Length; i++)
+            {
+                if (arrVers1[i] == arrVers2[i])
+                {
+                    continue;
+                }
+                if (arrVers1[i] > arrVers2[i])
+                {
+                    return 1;
+                }
+                return -1;
+            }
+            return 0;
+        }
+
+        private static void AlignLength(int[] array, int size)
+        {
+            int previousSize = array.Length;
+            Array.Resize<int>(ref array, size);
+            for (int i = previousSize; i < size; i++)
+            {
+                array[i] = 0;
+            }
+        }
+    }
+}
---
+++ Lab1/Lab1/UnitTest.cs
@@ -0,0 +1,94 @@
+﻿using NUnit.Framework;
+
+namespace QA_Lab1
+{
+    [TestFixture]
+    public class TestsQueue
+    {
+
+        [Test]
+        public void Queue_Enqueue_ShouldReturn2()
+        {
+            Queue<int> testQueue = new Queue<int>();
+            testQueue.Enqueue(-3);
+            testQueue.Enqueue(1567);
+            Assert.AreEqual(testQueue.Count, 2);
+        }
+
+        [Test]
+        public void Queue_Dequeue_ShouldReturn3()
+        {
+            Queue<int> testQueue = new Queue<int>();
+            testQueue.Enqueue(-120);
+            testQueue.Enqueue(134);
+            testQueue.Enqueue(3454);
+            testQueue.Enqueue(1312);
+            testQueue.Enqueue(-1312);
+
+            testQueue.Dequeue();
+            testQueue.Dequeue();
+            Assert.AreEqual(testQueue.Count, 3);
+        }
+
+        [Test]
+        public void Queue_Dequeue_ShouldReturn134()
+        {
+            Queue<int> testQueue = new Queue<int>();
+            testQueue.Enqueue(-120);
+            testQueue.Enqueue(134);
+            testQueue.Enqueue(3454);
+            testQueue.Enqueue(1312);
+            testQueue.Enqueue(-1312);
+
+            testQueue.Dequeue();
+            int item = testQueue.Dequeue();
+            Assert.AreEqual(item, 134);
+        }
+
+        [Test]
+        public void Queue_Peek_ShouldReturn1000()
+        {
+            Queue<int> testQueue = new Queue<int>();
+            testQueue.Enqueue(1000);
+            testQueue.Enqueue(134);
+            testQueue.Enqueue(3454);
+            testQueue.Enqueue(1312);
+            testQueue.Enqueue(-1312);
+
+            testQueue.Peek();
+            int item = testQueue.Peek();
+            Assert.AreEqual(item, 1000);
+        }
+
+        [Test]
+        public void Queue_Clear_ShouldReturn5()
+        {
+            Queue<int> testQueue = new Queue<int>();
+            testQueue.Enqueue(-120);
+            testQueue.Enqueue(134);
+            testQueue.Enqueue(3454);
+            testQueue.Enqueue(1312);
+            testQueue.Enqueue(-1312);
+
+            testQueue.Clear();
+            Assert.AreEqual(testQueue.Count, 0);
+        }
+    }
+
+    [TestFixture]
+    public class TestsVersionCompare
+    {
+        [TestCase("1.2.8.0", "1.2.7.4", 1)]
+        [TestCase("2.2.7.0.4.6", "2.2.7.4.0", -1)]
+        [TestCase("8.9.1", "8.9.1.0", 0)]
+        [TestCase("1.2.3", "4.5.6", -1)]
+        [TestCase("1", "1.0", 0)]
+        [TestCase("1.1.0", "1.0.1", 1)]
+        public void VersionCompare_Compare(string firstString, string secondString, int expectedResult)
+        {
+            int actualResult = VersionCompare.Compare(firstString, secondString);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+    }
+}
