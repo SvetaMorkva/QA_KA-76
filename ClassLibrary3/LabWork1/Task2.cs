8980cc92faa1bb6b6effc51203e308cce77f7ffd
bataisk <donnatroal@gmail.com>
2020-04-22 16:51:50 +0300

    Completed task2

---
+++ ClassLibrary3/LabWork1/Task2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace ClassLibrary3
+{
+    public class Task2
+    {
+        public int CompareVersions(String version1, String version2)
+        {
+            for (int i = 0; i < Math.Max(version1.Split('.').Length, version2.Split('.').Length); i++)
+            {
+                if ((i < version1.Split('.').Length) && (i < version2.Split('.').Length))
+                {
+                    if (Int32.Parse(version1.Split('.')[i]) > Int32.Parse(version2.Split('.')[i]))
+                    {
+                        return 1;
+                    }
+                    else if (Int32.Parse(version1.Split('.')[i]) < Int32.Parse(version2.Split('.')[i]))
+                    {
+                        return -1;
+                    }
+
+                }
+                else if (i >= version1.Split('.').Length)
+                {
+                    if (0 > Int32.Parse(version2.Split('.')[i]))
+                    {
+                        return 1;
+                    }
+                    else if (0 < Int32.Parse(version2.Split('.')[i]))
+                    {
+                        return -1;
+                    }
+                }
+
+                else if (i >= version2.Split('.').Length)
+                {
+                    if (0 > Int32.Parse(version1.Split('.')[i]))
+                    {
+                        return -1;
+                    }
+                    else if (0 < Int32.Parse(version1.Split('.')[i]))
+                    {
+                        return 1;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
--- ClassLibrary3/LabWork1/Tests.cs
+++ ClassLibrary3/LabWork1/Tests.cs
@@ -3,7 +3,7 @@
 
 namespace ClassLibrary3
 {
-    class UnitTests1
+    class UnitTestsForTask1
     {
         Task1[] mocks = new Task1[2];
 
@@ -38,6 +38,22 @@ namespace ClassLibrary3
             DoubleLinkedList<int> testList = mocks[1].TestList;
             testList.Add(-60);
             Assert.AreEqual(-60, testList.GetNext(4).Data);
+        }
+    }
+
+    class UnitTestsForTask2
+    {
+        [TestCase("1.2.4", "5.2.1", -1)]
+        [TestCase("5.2.1", "1.1.1.1.1", 1)]
+        [TestCase("1.2", "1.2.1", -1)]
+        [TestCase("1.2.4.4.5.6.7.7.1", "1.2", 1)]
+        [TestCase("1.2.0.0.0.0.0.0", "1.2", 0)]
+
+        public void TestTask2(string version1, string version2, int result)
+        {
+            Task2 versionComparator = new Task2();
+            int trueResult = versionComparator.CompareVersions(version1, version2);
+            Assert.AreEqual(trueResult, result);
         }
     }
 }
