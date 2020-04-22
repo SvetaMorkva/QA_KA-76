40887072104c5d991b18d49d96f53ec82df0d4f3
bataisk <donnatroal@gmail.com>
2020-04-22 16:44:48 +0300

    Completed task1

---
+++ ClassLibrary3/LabWork1/DoubleLinkedList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+namespace ClassLibrary3
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+
+        public Node<T> Next { get; set; }
+        public Node<T> Previous { get; set; }
+
+        public Node(T value)
+        {
+            Data = value;
+        }
+    }
+
+    public class DoubleLinkedList<T>
+    {
+        private Node<T> Head { get; set; }
+
+
+        public void Add(T value)
+        {
+            if (Head == null)
+            {
+                Node<T> newNode = new Node<T>(value);
+                newNode.Next = null;
+                newNode.Previous = null;
+
+                Head = newNode;
+            }
+            else
+            {
+                Node<T> CurrentNode = Head;
+                while (CurrentNode.Next != null)
+                {
+                    CurrentNode = CurrentNode.Next;
+                }
+                Node<T> newNode = new Node<T>(value);
+                newNode.Next = null;
+                newNode.Previous = CurrentNode;
+                CurrentNode.Next = newNode;
+            }
+        }
+
+        public Node<T> GetCurrent(int index)
+        {
+            if (index < 0)
+            {
+                throw new Exception("Provide a valid index.");
+            }
+            Node<T> CurrentNode = Head;
+            for (int i = 0; i < index; i++)
+            {
+                if (CurrentNode.Next != null)
+                {
+                    CurrentNode = CurrentNode.Next;
+                }
+                else if ((CurrentNode.Next == null) && (i == index))
+                {
+                    return CurrentNode;
+                }
+                else
+                {
+                    throw new Exception("Provide a valid index.");
+                }
+            }
+            return CurrentNode;
+        }
+        public Node<T> GetPrevious(int index)
+        {
+            return GetCurrent(index - 1);
+        }
+        public Node<T> GetNext(int index)
+        {
+            return GetCurrent(index + 1);
+        }
+
+    }
+}
---
+++ ClassLibrary3/LabWork1/Task1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3
+{
+  
+    public class Task1
+    {
+        public DoubleLinkedList<int> TestList { get; }
+
+        public Task1(int n = 1)
+        {
+            TestList = new DoubleLinkedList<int>();
+
+            if (n == 1)
+            {
+                TestList.Add(10);
+                TestList.Add(20);
+                TestList.Add(30);
+                TestList.Add(40);
+                TestList.Add(50);
+            }
+            else
+            {
+                TestList.Add(-10);
+                TestList.Add(-20);
+                TestList.Add(-30);
+                TestList.Add(-40);
+                TestList.Add(-50);
+            }
+        }
+    }
+  
+}
