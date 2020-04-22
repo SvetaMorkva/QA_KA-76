1cf7ff9dc5b72ab9848f46137032ce7f22e41def
missdaria77 <mischenko_dasha@ukr.net>
2020-04-22 17:24:17 +0300

    Done

---
+++ lab1/DoubleLinkedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace lab1
+{
+    public class Node<T>
+    {
+        public T item { get; set; }
+        public Node<T> Next { get; set; }
+        public Node<T> Previous { get; set; }
+
+        public Node(T item)
+        {
+            this.item = item;
+        }
+    }
+
+    public class DoubleLinkedList<T>
+    {
+        private Node<T> head { get; set; }
+        private Node<T> tail { get; set; }
+        private Node<T> current { get; set; }
+
+        public void Add(T item)
+        {
+            if (head == null)
+            {
+                head = new Node<T>(item);
+                current = head;
+            } 
+            else if (tail == null)
+            {
+                tail = new Node<T>(item);
+                head.Next = tail;
+                tail.Previous = head;
+            }
+            else
+            {
+                Node<T> node = new Node<T>(item);
+                node.Previous = tail;
+                tail.Next = node;
+                tail = node;
+            }
+        }
+
+        public T GetCurrent()
+        {
+            return current.item;
+        }
+        public T GetPrevious()
+        {
+            current = current.Previous;
+            return current.item;
+        }
+        public T GetNext()
+        {
+            current = current.Next;
+            return current.item;
+        }
+
+    }
+}
---
+++ lab1/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+
+namespace lab1
+{
+    public class UnitTest1
+    {
+
+        [Test]
+        public void Test_Add()
+        {
+            DoubleLinkedList<string> list = new DoubleLinkedList<string>();
+            string str1 = "test1";
+            list.Add(str1);
+
+            Assert.AreEqual(list.GetCurrent(), str1);
+        }
+
+
+        [Test]
+        public void Test_GetCurrent()
+        {
+            DoubleLinkedList<string> list = new DoubleLinkedList<string>();
+            string str1 = "test1";
+            string str2 = "test2";
+            list.Add(str1);
+            list.Add(str2);
+
+            Assert.AreEqual(list.GetCurrent(), str1);
+        }
+
+
+        [Test]
+        public void Test_GetNext()
+        {
+            DoubleLinkedList<string> list = new DoubleLinkedList<string>();
+            string str1 = "test1";
+            string str2 = "test2";
+            list.Add(str1);
+            list.Add(str2);
+
+            Assert.AreEqual(list.GetNext(), str2);
+        }
+
+
+        [Test]
+        public void Test_GetPrevious()
+        {
+            DoubleLinkedList<string> list = new DoubleLinkedList<string>();
+            string str1 = "test1";
+            string str2 = "test2";
+            list.Add(str1);
+            list.Add(str2);
+            string skip = list.GetNext();
+
+            Assert.AreEqual(list.GetPrevious(), str1);
+        }
+
+
+
+        [TestCase("1.2.3", "4.5.6", -1)]
+        [TestCase("1", "1.0", 0)]
+        [TestCase("1.1.0", "1.0.1", 1)]
+        [TestCase("1.2.7.4.5.6", "1.2.7.4.5.4.5.6", 1)]
+        [TestCase("1.2.7.4.5.6", "1.2.7.4.5.6", 0)]
+        [TestCase("1.2.7.4.5.6", "1.3.1", -1)]
+        public void Test_VersionCompare(string str1, string str2, int expectedResult)
+        {
+            Task2 comp = new Task2();
+            int actualResult = comp.CompareVersions(str1, str2);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
