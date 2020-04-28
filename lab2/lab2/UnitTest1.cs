804801137e36d8b3f01b785b98a36105b7e0134c
Olha Pashnieva <olha.pashnieva@gmail.com>
2020-04-28 18:10:58 +0300

    selenium. login to HubSpot

--- lab2/lab2/UnitTest1.cs
+++ lab2/lab2/UnitTest1.cs
@@ -1,4 +1,7 @@
 using NUnit.Framework;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace lab2
 {
@@ -7,6 +10,18 @@ namespace lab2
         [SetUp]
         public void Setup()
         {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://app.hubspot.com/login");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+
+            By usernamePath = By.XPath("//*[@id='username']");
+            By passwordPath = By.XPath("//*[@id='password']");
+            By loginBtnPath = By.XPath("//*[@id='loginBtn']");
+
+            // login to HubSpot
+            driver.FindElement(usernamePath).SendKeys("olha.pashnieva@gmail.com");
+            driver.FindElement(passwordPath).SendKeys("QALab123456");
+            driver.FindElement(loginBtnPath).Submit();
         }
 
         [Test]
