0c885a19fa22bb6c1fee0e418e99a50535ab0ee0
Daryna Mishchenko <mischenko_dasha@ukr.net>
2020-04-29 12:15:10 +0300

    Initial commit for lab2

---
+++ Lab2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab2")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d9696405-e0ba-4288-9059-49b870633916")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lab2/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Lab2
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
