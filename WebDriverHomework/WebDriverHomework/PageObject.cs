a6249a73549e0831cc36fbc36ef5b160a6b07312
Dmytro Ihnatov <ihnatov.d@gmail.com>
2020-04-29 17:53:45 +0300

    Initial commit. The very start of instagram UI testing

---
+++ WebDriverHomework/WebDriverHomework/PageObject.cs
@@ -0,0 +1,79 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace WebDriverHomework
+{
+    public class PageObject
+    {
+        private IWebDriver driver;
+
+        public PageObject(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+
+        [FindsBy(How = How.CssSelector, Using = ".TqC_a")]
+        private IWebElement search { get; }
+
+        //navbar
+        //.s4Iyt
+        [FindsBy(How = How.CssSelector, Using = ".Fifk5:nth-child(1) a")]
+        private IWebElement homeButton;
+        //.xWeGp
+        [FindsBy(How = How.CssSelector, Using = ".Fifk5:nth-child(2) a")]
+        private IWebElement inboxButton;
+
+        [FindsBy(How = How.CssSelector, Using = ".Fifk5:nth-child(3) a")]
+        private IWebElement exploreButton;
+
+        [FindsBy(How = How.CssSelector, Using = ".Fifk5:nth-child(4) a")]
+        private IWebElement activityButton;
+
+        [FindsBy(How = How.CssSelector, Using = ".Fifk5:nth-child(5) a")]
+        private IWebElement profileButton;
+
+
+        public PageObject openHomePage()
+        {
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementToBeClickable(homeButton));
+            homeButton.Click();
+            return this;
+        }
+
+        public PageObject openInboxPage()
+        {
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementToBeClickable(inboxButton));
+            inboxButton.Click();
+            return this;
+        }
+
+        public PageObject openExplorePage()
+        {
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementToBeClickable(exploreButton));
+            exploreButton.Click();
+            return this;
+        }
+
+        public PageObject openActivityPage()
+        {
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementToBeClickable(activityButton));
+            activityButton.Click();
+            return this;
+        }
+
+        public PageObject openProfilePage()
+        {
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementToBeClickable(profileButton));
+            profileButton.Click();
+            return this;
+        }
+
+
+
+    }
+}
+
---
+++ WebDriverHomework/WebDriverHomework/Tests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace WebDriverHomework
+{
+    [TestFixture]
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        private string url = "https://www.instagram.com/";
+
+        private string username = "brandd1re";
+        private string password = "ImaBitchImaBoss";
+
+        private string path = System.AppDomain.CurrentDomain.BaseDirectory + "Selenium";
+
+
+
+        public static IWebElement smartFind(IWebDriver driver, By selector)
+        {
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementExists(selector));
+            return driver.FindElement(selector);
+        }
+
+        public void waitUntilExists(IWebDriver driver, By selector)
+        {
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementExists(selector));
+        }
+
+
+
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            options.AddArguments(@"user-data-dir=" + path);
+
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl(url);
+
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(driver => driver.Url == url);
+
+
+            smartFind(driver, By.CssSelector(".FBi-h+ .-MzZI .zyHYP")).SendKeys(username);
+            smartFind(driver, By.CssSelector(".-MzZI+ .-MzZI .zyHYP")).SendKeys(password);
+            smartFind(driver, By.CssSelector(".-MzZI+ .DhRcB")).Click();
+
+            waitUntilExists(driver, By.CssSelector(".Fifk5 ._6q-tv"));
+            
+
+            var mainPage = new PageObject(driver);
+
+            driver.SwitchTo().Window(driver.WindowHandles.First());
+        }
+
+        [TearDown]
+        public void TestFinalize()
+        {
+            driver.Quit();
+        }
+
+
+    }
+}
