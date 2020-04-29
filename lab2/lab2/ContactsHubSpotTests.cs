850837063b8baa9c13f04f5382bb3b3b51d230e7
Olha Pashnieva <olha.pashnieva@gmail.com>
2020-04-29 18:03:02 +0300

    now 4 working tests in total

---
+++ lab2/lab2/ContactsHubSpotTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace lab2
+{
+    public class ContactsTests
+    {
+        private IWebDriver driver;
+        private readonly string email = "olha.pashnieva@gmail.com";
+        private readonly string password = "QALab123456";
+
+        private readonly By createContactButton = By.CssSelector("button[data-selenium-test='new-object-button']");
+        private readonly By contactEmailInput = By.CssSelector("input[data-selenium-test='property-input-email']");
+        private readonly By createButton = By.CssSelector("button[data-selenium-test='base-dialog-confirm-btn']");
+
+        private readonly string randomStr = Path.GetRandomFileName().Replace(".", "");
+
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://app.hubspot.com/login");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+
+            // login to HubSpot
+            driver.FindElement(By.Id("username")).SendKeys(email);
+            driver.FindElement(By.Id("password")).SendKeys(password);
+            driver.FindElement(By.Id("loginBtn")).Submit();
+
+            // go to Contacts page
+            driver.FindElement(By.Id("nav-primary-contacts-branch")).Click();
+            driver.FindElement(By.Id("nav-secondary-contacts")).Click();
+        }
+
+        [Test]
+        public void CreateContactTroughEmailOnly()
+        {
+            string contactEmail = randomStr + "@me.com";
+
+            driver.FindElement(createContactButton).Click();
+            driver.FindElement(contactEmailInput).SendKeys(contactEmail);
+
+            // var createButtonClickable = waiterVar.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(createButton));
+            // createButtonClickable.Click();
+            System.Threading.Thread.Sleep(1000);
+            driver.FindElement(createButton).Click();
+
+            // verify contact is created
+            By createdContactEmailSpan = By.CssSelector("span[data-selenium-test='highlightTitle']>span");
+            string createdContactEmail = driver.FindElement(createdContactEmailSpan).GetAttribute("textContent");
+
+            Assert.AreEqual(contactEmail, createdContactEmail);
+        }
+
+        [Test]
+        public void AttemptCreatingContactWithInvalidEmail()
+        {
+            driver.FindElement(createContactButton).Click();
+            driver.FindElement(contactEmailInput).SendKeys(randomStr);
+            driver.FindElement(createButton).Click();
+
+            By errorElem = By.CssSelector("i18n-string[data-key='customerDataProperties.PropertyInput.errorMessageInvalidEmail']");
+            System.Collections.ObjectModel.ReadOnlyCollection<IWebElement> invalidEmailError = driver.FindElements(errorElem);
+            Assert.AreEqual(1, invalidEmailError.Count());
+        }
+
+
+    }
+}
--- lab2/lab2/GeneralHubSpotTests.cs
+++ lab2/lab2/GeneralHubSpotTests.cs
@@ -1,23 +1,28 @@
 using NUnit.Framework;
-using System;
-using System.IO;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
 
 namespace lab2
 {
-    public class Tests
+    public class GeneralTests
     {
         private IWebDriver driver;
         private readonly string email = "olha.pashnieva@gmail.com";
         private readonly string password = "QALab123456";
+
+        private readonly string randomStr = Path.GetRandomFileName().Replace(".", "");
+        // private WebDriverWait waiterVar;
 
         [SetUp]
         public void Setup()
         {
             driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://app.hubspot.com/login");
+
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            // waiterVar = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
 
             // login to HubSpot
             driver.FindElement(By.Id("username")).SendKeys(email);
@@ -26,7 +31,7 @@ namespace lab2
         }
 
         [Test]
-        public void CreateATask()
+        public void CreateTask()
         {
             // go to Tasks page
             driver.FindElement(By.Id("nav-primary-sales-branch")).Click();
@@ -37,10 +42,8 @@ namespace lab2
             By taskTitleInput = By.CssSelector("input[data-selenium-test='property-input-hs_task_subject']");
             By createButton = By.CssSelector("button[data-selenium-test='CreateTaskSidebar__save-btn']");
 
-            string taskTittle = Path.GetRandomFileName().Replace(".", ""); // get random string for the task name
-
             driver.FindElement(createTaskButton).Click();
-            driver.FindElement(taskTitleInput).SendKeys(taskTittle);
+            driver.FindElement(taskTitleInput).SendKeys(randomStr);
             driver.FindElement(createButton).Click();
 
             // verify task is created
@@ -48,7 +51,32 @@ namespace lab2
             By newestTaskTittle = By.CssSelector("a[data-selenium-test='task-subject-cell__link']>span>span>span");
             string laskTastTittle = driver.FindElement(newestTaskTittle).GetAttribute("textContent");
 
-            Assert.AreEqual(taskTittle, laskTastTittle);
+            Assert.AreEqual(randomStr, laskTastTittle);
+        }
+
+        [Test]
+        public void ChangeProfileName()
+        {
+            driver.FindElement(By.Id("navSetting")).Click(); // go to Settings page
+            driver.FindElement(By.CssSelector("a[data-selenium-id='Basic info']")).Click();
+            driver.FindElement(By.CssSelector("span[class='UIIcon__IconContent-sc-1ngbkfp-0 hJvPJd']")).Click();
+
+            string randomName = randomStr;
+            string randomSurname = randomStr + "Surname";
+            string fullRandomName = randomName + " " + randomSurname;
+
+            driver.FindElement(By.CssSelector("input[data-test-id='first-name']")).Clear();
+            driver.FindElement(By.CssSelector("input[data-test-id='last-name']")).Clear();
+
+            driver.FindElement(By.CssSelector("input[data-test-id='first-name']")).SendKeys(randomName);
+            driver.FindElement(By.CssSelector("input[data-test-id='last-name']")).SendKeys(randomSurname);
+
+            driver.FindElement(By.CssSelector("button[data-test-id='save-name']")).Click();
+
+            System.Threading.Thread.Sleep(1000);
+            string newName = driver.FindElement(By.CssSelector("span[data-test-id='name']")).GetAttribute("innerHTML");
+
+            Assert.AreEqual(fullRandomName, newName);
         }
     }
 }
