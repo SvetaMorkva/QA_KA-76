49589c61f3bec77d617dfd4515081af1fd544d1e
Artur <artur.borbela1122@gmail.com>
2020-04-30 12:12:31 +0300

    Change adding try catch to world.js

--- untitled2/features/step_difinitions/world.js
+++ untitled2/features/step_difinitions/world.js
@@ -6,15 +6,20 @@ setDefaultTimeout(60 * 1000);
 
 
 function CustomWorld() {
-    this.driver = new seleniumWebdriver.Builder()
-        .forBrowser('firefox')
-        .build()
+    try {
+        this.driver = new seleniumWebdriver.Builder()
+            .forBrowser('firefox')
+            .build()
 
-    // Returns a promise that resolves to the element
-    this.waitForElement = function(locator) {
-        const condition = seleniumWebdriver.until.elementLocated(locator)
-        return this.driver.wait(condition)
+        // Returns a promise that resolves to the element
+        this.waitForElement = function(locator) {
+            const condition = seleniumWebdriver.until.elementLocated(locator)
+            return this.driver.wait(condition)
+        }
+    } catch (e) {
+        console.log("Error create webdriver");
     }
+
 }
 
 setWorldConstructor(CustomWorld);
