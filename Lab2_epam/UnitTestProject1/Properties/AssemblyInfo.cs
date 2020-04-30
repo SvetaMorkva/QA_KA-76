af6cc52d687d3483ddaa154d894df6c4a43cf506
Julia Tsymbal <6741017@gmail.com>
2020-04-30 22:04:29 +0300

    first commit

---
+++ Lab2_epam/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2df00ca4-4912-406f-99d4-04a4f67c8357")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lab2_epam/UnitTestProject1/UnitTests.cs
@@ -0,0 +1,179 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using System.Threading;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Lab2
+{
+    [TestFixture]
+    public class Tests
+    {
+        private IWebDriver driver;
+        private string _url = "https://nszu.gov.ua/";
+
+        [Obsolete]
+        private IWebElement WaitForFindElement(IWebDriver driver, By selector)
+        {
+            new WebDriverWait(driver, TimeSpan.FromSeconds(60)).Until(ExpectedConditions.ElementToBeClickable(selector));
+            return driver.FindElement(selector);
+        }
+
+        [Obsolete]
+        [SetUp]
+        public void TestInitialize()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            options.AddArgument("proxy-server='direct://'");
+            options.AddArgument("proxy-bypass-list=*");
+
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl(_url);
+            new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(d => d.Url == _url);
+            
+        }
+
+        [Obsolete]
+        [TearDown]
+        public void TestFinalize()
+        {
+            driver.Quit();
+        }
+
+        [Obsolete]
+        [TestCase("Контакти", "kontakti")]
+        [TestCase("Закупівлі", "zakupivli")]
+        public void TestWorkOfNavigation(string infoUkr, string infoLatin)
+        {
+            string infoUkr1 = infoUkr.ToLower();
+            WaitForFindElement(driver, By.Id("foo")).Click();
+            WaitForFindElement(driver, By.XPath($"//div[@class='col-12 col-md-6 col-xl-4 jj']//ul//li//ul//a[contains(text(), '{infoUkr}')]")).Click();
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            Thread.Sleep(3000);
+            string actual_label = WaitForFindElement(driver, By.CssSelector(".main-wrapper h1")).Text.ToLower();
+            
+            using (new AssertionScope())
+            {
+                actual_label.Should().Contain(infoUkr1);
+                driver.Url.Should().Be($"https://nszu.gov.ua/pro-nszu/{infoLatin}");
+            }
+        }
+        
+
+        [Obsolete]
+        [Test]
+        public void TestGoToAcademy()
+        {
+            WaitForFindElement(driver, By.CssSelector("a[href*=\"nszu.gov.ua/academy\"]")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.CssSelector(".col-md-5 a")).Click();
+            Thread.Sleep(2000);
+
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            string actualPoint = WaitForFindElement(driver, By.CssSelector(".sign-up-btn button")).Text.ToLower();
+
+            using (new AssertionScope())
+            {
+                actualPoint.Should().Be("вхід");
+                driver.Url.Should().Be("https://academy.nszu.gov.ua/");
+
+            }
+        }
+
+
+        [Obsolete]
+        [Test]
+        public void TestSwitchLangToEng()
+        {
+            string ukr_label = WaitForFindElement(driver, By.ClassName("news-home-description")).Text;
+            WaitForFindElement(driver, By.CssSelector(".lang-item-bottom a")).Click();
+            Thread.Sleep(1000);
+            string eng_label = WaitForFindElement(driver, By.ClassName("news-home-description")).Text;
+
+            using (new AssertionScope())
+            {
+                ukr_label.Should().Contain("далі");
+                eng_label.Should().Contain("Read");
+                driver.Url.Should().Be("https://nszu.gov.ua/en");
+            }
+        }
+
+
+        [Obsolete]
+        [Test]
+        public void TestOpenFacebookSource()  // в Feature проверялась ссылка на Telegram, но в этом месте на сайте баг, тест не прошел бы, заменила на Facebook
+        {
+            WaitForFindElement(driver, By.CssSelector(".heade__icon.f-heade__icon a")).Click();
+            Thread.Sleep(3000);
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            
+
+            using (new AssertionScope())
+            {
+                driver.Url.Should().Be("https://www.facebook.com/nszu.ukr/");
+            }
+        }
+        
+
+        [Obsolete]
+        [Test]
+        public void TestGetSearchBoxResult()
+        {
+            string textValueToSet = "коронавірус";
+            driver.FindElement(By.CssSelector(".search-container.for-desktop input")).SendKeys(textValueToSet);
+            WaitForFindElement(driver, By.CssSelector(".search-container.for-desktop button")).Click();
+            Thread.Sleep(1000);
+            string actualArticle = WaitForFindElement(driver, By.CssSelector(".main-wrapper h1")).Text.ToLower();
+
+            using (new AssertionScope())
+            {
+                actualArticle.Should().Be("результати пошуку");
+            }
+        }
+   
+
+        [Obsolete]
+        [Test]
+        public void TestGetReports()
+        {
+            WaitForFindElement(driver, By.Id("foo")).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.CssSelector("a[href*=\"zviti\"]")).Click();
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            string actualPoint = WaitForFindElement(driver, By.CssSelector(".main-wrapper h1")).Text.ToLower();
+
+            using (new AssertionScope())
+            {
+                actualPoint.Should().Be("звіти");
+            }
+        }
+        
+
+        [Obsolete]
+        [Test]
+        public void TestGetTelephoneForFeetback()
+        {
+            string actual_number = WaitForFindElement(driver, By.CssSelector("a[href*=\"tel:16-77\"]")).Text;
+
+            using (new AssertionScope())
+                actual_number.Should().Be("16-77");
+        }
+        
+         
+        [Obsolete]
+        [Test]
+        public void TestGetEmailForFeetback()
+        {
+            string actual_email = WaitForFindElement(driver, By.CssSelector("a[href*=\"info@nszu.gov.ua\"]")).Text;
+
+            using (new AssertionScope())
+                actual_email.Should().Be("info@nszu.gov.ua");
+        }
+    }
+}
