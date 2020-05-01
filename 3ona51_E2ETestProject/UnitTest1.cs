bdb2b9e2f497a1545202d1cc4b1e0daf68dad8a8
oYPIo <37488187+oYPIo@users.noreply.github.com>
2020-05-01 19:53:13 +0300

    Initial commit

---
+++ 3ona51_E2ETestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace _3ona51_E2ETestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
