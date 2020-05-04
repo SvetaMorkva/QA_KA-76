87a32e01922ac79ba87684443e6ddf8454689e00
oYPIo <37488187+oYPIo@users.noreply.github.com>
2020-05-04 19:09:54 +0300

    Add LikePage PageObject Test

---
+++ SoundCloud_E2ETestProject/FeaturesTest.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace SoundCloud_E2ETestProject
+{
+    class ScrollListOfPlaylistsFeatureTest
+    {
+
+    }
+}
--- SoundCloud_E2ETestProject/NUnitTest.cs
+++ SoundCloud_E2ETestProject/NUnitTest.cs
@@ -9,6 +9,7 @@ namespace SoundCloud_E2ETestProject
     {
         private IWebDriver _driver;
         private string _url = "https://soundcloud.com/discover";
+        private int sleepTime = 6000;
 
         [SetUp]
         public void Setup()
@@ -24,7 +25,7 @@ namespace SoundCloud_E2ETestProject
         public void GoToPlaylistPage(int indexInPlaulistList)
         {
             HomePage homePage = new HomePage(_driver);
-            Thread.Sleep(6000);
+            Thread.Sleep(sleepTime);
             _driver.Navigate().GoToUrl(homePage.GetPlaylistPageURL(indexInPlaulistList));
             Assert.Pass();
         }
@@ -34,7 +35,7 @@ namespace SoundCloud_E2ETestProject
         public void ScrollListOfPlaylists_FarwardScroll(int indexOfListOfPlaylists)
         {
             HomePage playlistPage = new HomePage(_driver);
-            Thread.Sleep(6000);
+            Thread.Sleep(sleepTime);
             playlistPage.ScrollListOfPlaylistFarward(indexOfListOfPlaylists);
             Assert.Pass();
         }
@@ -44,7 +45,7 @@ namespace SoundCloud_E2ETestProject
         public void ScrollListOfPlaylists_FarwardAndBackwardScroll(int indexOfListOfPlaylists)
         {
             HomePage playlistPage = new HomePage(_driver);
-            Thread.Sleep(6000);
+            Thread.Sleep(sleepTime);
             playlistPage.ScrollListOfPlaylistFarward(indexOfListOfPlaylists);
             playlistPage.ScrollListOfPlaylistBackward(indexOfListOfPlaylists);
             Assert.Pass();
@@ -62,6 +63,7 @@ namespace SoundCloud_E2ETestProject
     {
         private IWebDriver _driver;
         private string _url = "https://soundcloud.com/user-876658341/sets/sunday-music-and-hot-coffee";
+        private int sleepTime = 6000;
 
         [SetUp]
         public void Setup()
@@ -76,7 +78,7 @@ namespace SoundCloud_E2ETestProject
         public void TurnOnTrack(int indexInTrackList)
         {
             PlaylistPage playlistPage = new PlaylistPage(_driver);
-            Thread.Sleep(6000);
+            Thread.Sleep(sleepTime);
             playlistPage.PlayTrack(indexInTrackList);
             Assert.Pass();
         }
@@ -87,7 +89,7 @@ namespace SoundCloud_E2ETestProject
         public void GoToTrackPage(int indexInTrackList)
         {
             PlaylistPage playlistPage = new PlaylistPage(_driver);
-            Thread.Sleep(6000);
+            Thread.Sleep(sleepTime);
             _driver.Navigate().GoToUrl(playlistPage.GetTrackPageURL(indexInTrackList));
             Assert.Pass();
         }
@@ -103,6 +105,7 @@ namespace SoundCloud_E2ETestProject
     {
         private IWebDriver _driver;
         private string _url = "https://soundcloud.com/ruucampbell/crossroads-heartsong-ruu?in=user-876658341/sets/sunday-music-and-hot-coffee";
+        private int sleepTime = 6000;
 
         [SetUp]
         public void Setup()
@@ -115,8 +118,39 @@ namespace SoundCloud_E2ETestProject
         public void GoToLikePage()
         {
             TrackPage trackPage = new TrackPage(_driver);
-            Thread.Sleep(6000);
+            Thread.Sleep(sleepTime);
             _driver.Navigate().GoToUrl(trackPage.GetLikePageURL());
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _driver.Quit();
+        }
+    }
+
+    public class LikePageTest
+    {
+        private IWebDriver _driver;
+        private string _url = "https://soundcloud.com/ruucampbell/crossroads-heartsong-ruu/likes";
+        private int sleepTime = 6000;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(_url);
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        public void GoToUserWhoLikedPage(int indexOfUserWhoLiked)
+        {
+            LikePage likePage = new LikePage(_driver);
+            Thread.Sleep(sleepTime);
+            _driver.Navigate().GoToUrl(likePage.GetUserPage(indexOfUserWhoLiked));
             Assert.Pass();
         }
 
