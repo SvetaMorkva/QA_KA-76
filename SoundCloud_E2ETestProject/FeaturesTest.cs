e63d4c401b895016c50fc85b5a6c24d2e9dfc14b
oYPIo <37488187+oYPIo@users.noreply.github.com>
2020-05-04 20:30:54 +0300

    Add SelectUserWhoLikeTheTrack feature Test

--- SoundCloud_E2ETestProject/FeaturesTest.cs
+++ SoundCloud_E2ETestProject/FeaturesTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace SoundCloud_E2ETestProject
@@ -17,6 +18,8 @@ namespace SoundCloud_E2ETestProject
         {
             _driver = new ChromeDriver();
             _driver.Navigate().GoToUrl(_url);
+            CookiesNotification cookiesNotification = new CookiesNotification(_driver);
+            cookiesNotification.AllowCookies();
         }
 
         [TestCase(0)]
@@ -27,6 +30,50 @@ namespace SoundCloud_E2ETestProject
             Thread.Sleep(sleepTime);
             playlistPage.ScrollListOfPlaylistFarward(indexOfListOfPlaylists);
             playlistPage.ScrollListOfPlaylistBackward(indexOfListOfPlaylists);
+            Assert.Pass();
+        }
+
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _driver.Quit();
+        }
+    }
+
+    class SelectUserWhoLikedTheTrackFeatureTest
+    {
+
+        private IWebDriver _driver;
+        private string _url = "https://soundcloud.com/discover";
+        private int sleepTime = 6000;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(_url);
+            CookiesNotification cookiesNotification = new CookiesNotification(_driver);
+            cookiesNotification.AllowCookies();
+        }
+
+        [TestCase(0, 0, 0)]
+        [TestCase(1, 0, 2)]
+        public void ShowUsersWhoHaveLikedTrack_AND_SelectUserWhoHaveLikedTrack(int indexOfPlaylist, int indexOfTrack, int indexOfUser)
+        {
+            HomePage homePage = new HomePage(_driver);
+            Thread.Sleep(sleepTime);
+            _driver.Navigate().GoToUrl(homePage.GetPlaylistPageURL(indexOfPlaylist));
+
+            PlaylistPage playlistPage = new PlaylistPage(_driver);
+            _driver.Navigate().GoToUrl(playlistPage.GetTrackPageURL(indexOfTrack));
+
+            TrackPage trackPage = new TrackPage(_driver);
+            _driver.Navigate().GoToUrl(trackPage.GetLikePageURL());
+
+            LikePage likePage = new LikePage(_driver);
+            _driver.Navigate().GoToUrl(likePage.GetUserPageURL(indexOfUser));
+
             Assert.Pass();
         }
 
