374e95c05bba8e86344d62000c0a3bd136bee762
oYPIo <37488187+oYPIo@users.noreply.github.com>
2020-05-04 18:34:59 +0300

    Add TrackPage PageObject Test

--- SoundCloud_E2ETestProject/NUnitTest.cs
+++ SoundCloud_E2ETestProject/NUnitTest.cs
@@ -5,46 +5,6 @@ using System.Threading;
 
 namespace SoundCloud_E2ETestProject
 {
-    public class PlaylistPageTest
-    {
-        private IWebDriver _driver;
-        private string _url = "https://soundcloud.com/user-876658341/sets/sunday-music-and-hot-coffee";
-
-        [SetUp]
-        public void Setup()
-        {
-            _driver = new ChromeDriver();
-            _driver.Navigate().GoToUrl(_url);
-        }
-
-        [TestCase(0)]
-        [TestCase(1)]
-        [TestCase(3)]
-        public void TurnOnTrack(int indexInTrackList)
-        {
-            PlaylistPage playlistPage = new PlaylistPage(_driver);
-            playlistPage.PlayTrack(indexInTrackList);
-            Assert.Pass();
-        }
-
-        [TestCase(0)]
-        [TestCase(1)]
-        [TestCase(3)]
-        public void GoToTrackPage(int indexInTrackList)
-        {
-            PlaylistPage playlistPage = new PlaylistPage(_driver);
-            Thread.Sleep(6000);
-            _driver.Navigate().GoToUrl(playlistPage.GetTrackURL(indexInTrackList));
-            Assert.Pass();
-        }
-
-        [TearDown]
-        public void CleanUp()
-        {
-            _driver.Quit();
-        }
-    }
-
     public class HomePageTest
     {
         private IWebDriver _driver;
@@ -63,9 +23,9 @@ namespace SoundCloud_E2ETestProject
         [TestCase(3)]
         public void GoToPlaylistPage(int indexInPlaulistList)
         {
-            HomePage playlistPage = new HomePage(_driver);
+            HomePage homePage = new HomePage(_driver);
             Thread.Sleep(6000);
-            _driver.Navigate().GoToUrl(playlistPage.GetPlaylistURL(indexInPlaulistList));
+            _driver.Navigate().GoToUrl(homePage.GetPlaylistPageURL(indexInPlaulistList));
             Assert.Pass();
         }
 
@@ -97,4 +57,75 @@ namespace SoundCloud_E2ETestProject
             _driver.Quit();
         }
     }
+
+    public class PlaylistPageTest
+    {
+        private IWebDriver _driver;
+        private string _url = "https://soundcloud.com/user-876658341/sets/sunday-music-and-hot-coffee";
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(_url);
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(3)]
+        public void TurnOnTrack(int indexInTrackList)
+        {
+            PlaylistPage playlistPage = new PlaylistPage(_driver);
+            Thread.Sleep(6000);
+            playlistPage.PlayTrack(indexInTrackList);
+            Assert.Pass();
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(3)]
+        public void GoToTrackPage(int indexInTrackList)
+        {
+            PlaylistPage playlistPage = new PlaylistPage(_driver);
+            Thread.Sleep(6000);
+            _driver.Navigate().GoToUrl(playlistPage.GetTrackPageURL(indexInTrackList));
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _driver.Quit();
+        }
+    }
+
+    public class TrackPageTest
+    {
+        private IWebDriver _driver;
+        private string _url = "https://soundcloud.com/ruucampbell/crossroads-heartsong-ruu?in=user-876658341/sets/sunday-music-and-hot-coffee";
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(_url);
+        }
+
+        [Test]
+        public void GoToLikePage()
+        {
+            TrackPage trackPage = new TrackPage(_driver);
+            Thread.Sleep(6000);
+            _driver.Navigate().GoToUrl(trackPage.GetLikePageURL());
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _driver.Quit();
+        }
+    }
+
+
 }
---
+++ SoundCloud_E2ETestProject/TrackPage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+
+namespace SoundCloud_E2ETestProject
+{
+    class TrackPage
+    {
+        private IWebDriver _driver;
+        private WebDriverWait wait;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='app']/div[1]/div/div/div/button")]
+        private IWebElement cookiesButton { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "a.sc-ministats.sc-ministats-medium.sc-ministats-likes")]
+        private IWebElement LikeScoreLink { get; set; }
+
+        private IList<IWebElement> TrackPlayButton { get; set; }
+
+        public TrackPage(IWebDriver driver)
+        {
+            _driver = driver;
+            wait = new WebDriverWait(_driver, TimeSpan.FromMinutes(1));
+            PageFactory.InitElements(driver, this);
+
+            try
+            {
+                new WebDriverWait(driver, TimeSpan.FromMinutes(1)).Until(ExpectedConditions.ElementToBeClickable(cookiesButton));
+                cookiesButton.Click();
+            }
+            catch { }
+        }
+
+        private TrackPage GoToElement(IWebElement element)
+        {
+            Actions actions = new Actions(_driver);
+            actions.MoveToElement(element);
+            actions.Perform();
+            //scrall down in order to the MusicPlayerElement doesn't overlap the element
+            ((IJavaScriptExecutor)_driver).ExecuteScript("window.scrollBy(0,100)");
+
+            return this;
+
+        }
+
+        public string GetLikePageURL()
+        {
+            new WebDriverWait(_driver, TimeSpan.FromMinutes(1)).Until(ExpectedConditions.ElementToBeClickable(LikeScoreLink));
+            return LikeScoreLink.GetAttribute("href");
+
+        }
+
+    }
+}
