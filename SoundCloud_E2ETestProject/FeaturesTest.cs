3021ff3cf421ab4bec122f60f04216a16fc27114
oYPIo <37488187+oYPIo@users.noreply.github.com>
2020-05-04 19:19:33 +0300

    Add ScrollList feature

--- SoundCloud_E2ETestProject/FeaturesTest.cs
+++ SoundCloud_E2ETestProject/FeaturesTest.cs
@@ -8,5 +8,33 @@ namespace SoundCloud_E2ETestProject
     class ScrollListOfPlaylistsFeatureTest
     {
 
+        private IWebDriver _driver;
+        private string _url = "https://soundcloud.com/discover";
+        private int sleepTime = 6000;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(_url);
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        public void ScrollListOfPlaylists_FarwardAndBackwardScroll(int indexOfListOfPlaylists)
+        {
+            HomePage playlistPage = new HomePage(_driver);
+            Thread.Sleep(sleepTime);
+            playlistPage.ScrollListOfPlaylistFarward(indexOfListOfPlaylists);
+            playlistPage.ScrollListOfPlaylistBackward(indexOfListOfPlaylists);
+            Assert.Pass();
+        }
+
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _driver.Quit();
+        }
     }
 }
