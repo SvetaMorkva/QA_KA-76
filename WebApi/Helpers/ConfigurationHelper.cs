a1c7ec3b30555e16de4a4b692d9a8dc7052f50ae
Valeriia Khylenko <54900131+vkhylenko@users.noreply.github.com>
2020-05-29 22:11:45 +0300

    envToken fixed

--- WebApi/Helpers/ConfigurationHelper.cs
+++ WebApi/Helpers/ConfigurationHelper.cs
@@ -11,7 +11,7 @@ namespace WebApi.Helpers
     {
         public static string ServiceUrl => ConfigurationManager.AppSettings["serviceUrl"];
         public static string ContentServiceUrl => ConfigurationManager.AppSettings["contentServiceUrl"];
-        public static string AuthorizationToken => Environment.GetEnvironmentVariable(ConfigurationHelper.envTokenVarName);
+        public static string AuthorizationToken => Environment.GetEnvironmentVariable(ConfigurationManager.AppSettings["envTokenVarName"]);
         public static string DefaultFilePath => ConfigurationManager.AppSettings["defaultFilePath"];
 
     }
