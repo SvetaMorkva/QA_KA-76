a3734499b329a7cf2a297f2c595c4604fba409fb
Petro Chynnyk <chynnyk@vivaldi.net>
2020-05-31 02:04:47 +0300

    Duble7

--- WS/WS/TestDropboxApi/Helpers/ConfigurationHelper.cs
+++ WS/WS/TestDropboxApi/Helpers/ConfigurationHelper.cs
@@ -11,7 +11,7 @@ namespace TestDropboxApi.Helpers
     {
         public static string ServiceUrl => ConfigurationManager.AppSettings["serviceUrl"];
         public static string ContentServiceUrl => ConfigurationManager.AppSettings["contentServiceUrl"];
-        public static string AuthorizationToken => Environment.GetEnvironmentVariable(ConfigurationHelper.EnvAccessToken);
+        public static string AuthorizationToken => Environment.GetEnvironmentVariable("token");
         public static string DefaultFilePath => ConfigurationManager.AppSettings["defaultFilePath"];
 		 public static string EnvAccessToken => ConfigurationManager.AppSettings["envTokenVarName"];
 
