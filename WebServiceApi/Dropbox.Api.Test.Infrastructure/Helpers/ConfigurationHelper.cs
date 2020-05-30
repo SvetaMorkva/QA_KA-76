50549fb6f56d2976d64c92ae427b327d0d6d4639
Julia Tsymbal <6741017@gmail.com>
2020-05-30 12:42:54 +0300

    fixed variable

--- WebServiceApi/Dropbox.Api.Test.Infrastructure/Helpers/ConfigurationHelper.cs
+++ WebServiceApi/Dropbox.Api.Test.Infrastructure/Helpers/ConfigurationHelper.cs
@@ -11,7 +11,7 @@ namespace TestDropboxApi.Helpers
     {
         public static string ServiceUrl => ConfigurationManager.AppSettings["serviceUrl"];
         public static string ContentServiceUrl => ConfigurationManager.AppSettings["contentServiceUrl"];
-        public static string AuthorizationToken => Environment.GetEnvironmentVariable(ConfigurationManager.AppSettings["access_token"]);
+        public static string AuthorizationToken => Environment.GetEnvironmentVariable(ConfigurationManager.AppSettings["env.access_token"]);
         public static string DefaultFilePath => ConfigurationManager.AppSettings["defaultFilePath"];
 
     }
