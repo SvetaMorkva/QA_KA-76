35c2de66242a1406c5055412d80a1252aaaaaf19
Bohdan Lysov <BogUkraine@gmail.com>
2020-05-31 18:48:29 +0300

    rewriting comparator func

--- Lab1/Lab1/Comparator.cs
+++ Lab1/Lab1/Comparator.cs
@@ -10,32 +10,20 @@ namespace Lab1
     {
         public int CompareVersions(string str1, string str2)
         {
-            string[] split1 = str1.Trim().Split('.');
-            string[] split2 = str2.Trim().Split('.');
-            int length = Math.Max(split1.Length, split2.Length);
+            string strWithoutSpaces1 = str1.Trim();
+            string strWithoutSpaces2 = str2.Trim();
 
-            for (int i = 0; i < length; i++)
-            {
-                int num1;
-                if (split1.Length > i)
-                    num1 = int.Parse(split1[i]);
-                else
-                    num1 = 0;
+            var version1 = new Version(strWithoutSpaces1);
+            var version2 = new Version(strWithoutSpaces2);
 
-                int num2;
-                if (split2.Length > i)
-                    num2 = int.Parse(split2[i]);
-                else
-                    num2 = 0;
-
-
-                if (num1 > num2)
-                    return 1;
-                else if (num1 < num2)
-                    return -1;
-            }
-
-            return 0;
+            var result = version1.CompareTo(version2);
+            Console.WriteLine(result);
+            if (result > 0)
+                return 1;
+            else if (result < 0)
+                return -1;
+            else
+                return 0;
         }
     }
 }
--- Lab1/Lab1/Test1.cs
+++ Lab1/Lab1/Test1.cs
@@ -5,11 +5,10 @@ namespace Lab1
     class Test1
     {
         [TestCase("1.2.3", "4.5.6", -1)]
-        [TestCase("1", "1.0", 0)]
+        [TestCase("1.0", "1.0", 0)]
         [TestCase("1.1.0", "1.0.1", 1)]
-        [TestCase("1.2.7.4.5.6", "1.2.7.4.5.4.5.6", 1)]
-        [TestCase("1.2.7.4.5.6", "1.2.7.4.5.6", 0)]
-        [TestCase("1.2.7.4.5.6", "1.3.1", -1)]
+        [TestCase("1.2.0", "1.3.1", -1)]
+        [TestCase("1.4.0", "1.3.1", 1)]
         public void Test_VersionCompare(string firstString, string secondString, int expectedResult)
         {
             Comparator versionComparator = new Comparator();
